65991c7fd70a41ff3934e3738dd3d1da50bc0c28
Tomas Berger <tomashb@stud.ntnu.no>
2019-10-09 00:21:11 +0200

    Add logic to read drom file, still buggy

--- animal.go
+++ animal.go
@@ -3,21 +3,30 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
 	"strings"
-	"time"
 	"sync"
+	"time"
 )
 
 var startTime time.Time
+
+//VERSION The current api version
 const VERSION string = "v1"
+
+//MAXCALL How may calls we are going to make to the API
 const MAXCALL int = 5
+
+//GBIFOCCURANCESLIMIT What the limit for Gbif occurances is
 const GBIFOCCURANCESLIMIT int = 300
-const GBIFSPECIESLIMIT int = 1000;
+
+//GBIFSPECIESLIMIT What the limit for Gbif Speces is
+const GBIFSPECIESLIMIT int = 1000
 
 /*
 Country represents extint species by country
@@ -37,8 +46,8 @@ type CountryJSON struct {
 	CountryFlag string `json:"flag"`
 }
 
-//gbifJSON get the spices result
-type gbifJSON struct {
+//GbifJSON get the spices result
+type GbifJSON struct {
 	Results []SpeciesJSON `json:"results"`
 }
 
@@ -59,7 +68,7 @@ type Species struct {
 
 //SpeciesJSON represents a temporay struct to get data from GBIF
 type SpeciesJSON struct {
-	Key       int    `json:"speciesKey"`
+	Key       int    `json:"key"`
 	Kingdom   string `json:"kingdom"`
 	Phylum    string `json:"phylum"`
 	Order     string `json:"order"`
@@ -67,7 +76,6 @@ type SpeciesJSON struct {
 	Species   string `json:"species"`
 	SciName   string `json:"scientificName"`
 	CanonName string `json:"canonicalName"`
-	IsExtinct bool   `json:"extinct"`
 }
 
 /*
@@ -80,13 +88,14 @@ Diag represents some cool comments
 	}
 */
 type Diag struct {
-	Gbif          int     `json:"gbif"`
-	Restcountries int     `json:"restcountries"`
-	Version       string  `json:"version"`
+	Gbif          int    `json:"gbif"`
+	Restcountries int    `json:"restcountries"`
+	Version       string `json:"version"`
 	Uptime        string `json:"uptime"`
 }
+
 func uptime() time.Duration {
-    return time.Since(startTime)
+	return time.Since(startTime)
 }
 func nilHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Default Handler: Invalid request received.")
@@ -130,22 +139,33 @@ func speciesHandler(w http.ResponseWriter, r *http.Request) {
 		http.Header.Add(w.Header(), "content-type", "application/json")
 		parts := strings.Split(r.URL.Path, "/")
 		if len(parts) != 5 {
-			http.Error(w, "Expecting format .../country_ios2", http.StatusBadRequest)
+			http.Error(w, "Expecting format .../specieskey", http.StatusBadRequest)
 			return
 		}
 		if parts[4] == "" {
 			//Get all
 		} else if _, err := strconv.Atoi(parts[4]); err == nil {
-			query := "http://api.gbif.org/v1/occurrence/search?speciesKey=" + parts[4]
-			fmt.Println(query)
-			resp, err := http.Get(query)
+			file, err := os.Open("Species.txt")
 			if err != nil {
-				//We fucked up
+				log.Fatal(err)
 			}
-			data, _ := ioutil.ReadAll(resp.Body)
-			var speciesJSON gbifJSON
-			json.Unmarshal(data, &speciesJSON)
-			json.NewEncoder(w).Encode(speciesJSON)
+			defer file.Close()
+			data, err := ioutil.ReadAll(file)
+			i := 0
+			var species []SpeciesJSON
+			for {
+				var doc SpeciesJSON
+				err := json.Unmarshal(data, &doc)
+				if err == io.EOF {
+					break
+				}
+				if err != nil {
+					log.Fatal(err)
+				}
+				species[i] = doc
+				i++
+			}
+			json.NewEncoder(w).Encode(species)
 
 		} else {
 			fmt.Print(parts[4])
@@ -179,9 +199,9 @@ func diagHandler(w http.ResponseWriter, r *http.Request) {
 			fmt.Printf("The HTTP request failed with error %s\n", err)
 			return
 		}
-		
+
 		uptime := uptime()
-		uptimeString := fmt.Sprintf("%.0f seconds",uptime.Seconds()) 
+		uptimeString := fmt.Sprintf("%.0f seconds", uptime.Seconds())
 		diag := Diag{respneGbif.StatusCode, respneCon.StatusCode, VERSION, uptimeString}
 		json.NewEncoder(w).Encode(diag)
 		return
@@ -189,9 +209,32 @@ func diagHandler(w http.ResponseWriter, r *http.Request) {
 	http.Error(w, "only get method allowed", http.StatusNotImplemented)
 	return
 }
-func cachingSpecies(){
+func cachingSpecies() {
 	//TODO CreateFile
-	f, err := os.Create("Species2.txt")
+
+	file, err := os.Open("Species.txt")
+	if err != nil {
+		//Create the OS
+		file, err = os.Create("Species.txt")
+
+	} else {
+		//ut exsist and we need to see how old it is
+		info, err := os.Stat("Species.txt")
+		if err != nil {
+			//Send help
+		}
+		mtime := info.ModTime()
+		fmt.Println("Last changed:", mtime)
+		timenow := time.Now()
+		//Does not care for timezones btw
+		if timenow.Hour() > mtime.Hour()+24 {
+			fmt.Println("Cache is old, Run Update")
+
+		} else {
+			return
+		}
+	}
+
 	if err != nil {
 		fmt.Println("We fucked up")
 		//We fucked up
@@ -201,35 +244,39 @@ func cachingSpecies(){
 	var wg sync.WaitGroup
 	for i := 0; i < MAXCALL; i++ {
 		wg.Add(1)
-		go func() {
+		go func(i int) {
 			limit := strconv.Itoa(GBIFSPECIESLIMIT)
-			offset := strconv.Itoa(GBIFSPECIESLIMIT*i)
+			offset := strconv.Itoa(GBIFSPECIESLIMIT * i)
 			query := "http://api.gbif.org/v1/species/search?limit=" + limit + "&offset=" + offset
 			resp, err := http.Get(query)
 			if err != nil {
 				//We fucked up
 			}
 			data, _ := ioutil.ReadAll(resp.Body)
+			var results GbifJSON
+			//Get it in correct format
+			json.Unmarshal(data, &results)
+			data, _ = json.Marshal(results.Results)
 			datachan <- data
-		}() 
-		go func(){
-			getdata := <- datachan
-			f.Write(getdata)
+		}(i)
+		go func() {
+			getdata := <-datachan
+			file.Write(getdata)
 			fmt.Println("We have recived data")
 			wg.Done()
 		}()
 
-	} 
+	}
 	wg.Wait()
 	fmt.Println("We are done")
-	f.Close()
+	file.Close()
 }
-func init(){
+func init() {
 	startTime = time.Now()
 }
 func main() {
 	fmt.Println("Caching API's ")
-	cachingSpecies();
+	cachingSpecies()
 	fmt.Println("Starting application:")
 	port := os.Getenv("PORT")
 	if port == "" {
