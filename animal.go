9261758877712cf7df0a09301d5fbed6423db324
Tomas Berger <tomashb@ntnu.no>
2019-10-11 19:08:43 +1100

    Working species call

--- animal.go
+++ animal.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -11,16 +10,15 @@ import (
 	"strconv"
 	"strings"
 	"sync"
+	"sort"
 	"time"
 )
-
-var startTime time.Time
 
 //VERSION The current api version
 const VERSION string = "v1"
 
 //MAXCALL How may calls we are going to make to the API
-const MAXCALL int = 5
+const MAXCALL int = 2
 
 //GBIFOCCURANCESLIMIT What the limit for Gbif occurances is
 const GBIFOCCURANCESLIMIT int = 300
@@ -28,6 +26,11 @@ const GBIFOCCURANCESLIMIT int = 300
 //GBIFSPECIESLIMIT What the limit for Gbif Speces is
 const GBIFSPECIESLIMIT int = 1000
 
+//Species file name
+const FILENAME_S string = "Species.json"
+
+// Timm used for when the program start up
+var startTime time.Time
 /*
 Country represents extint species by country
 */
@@ -45,14 +48,19 @@ type CountryJSON struct {
 	CountryName string `json:"name"`
 	CountryFlag string `json:"flag"`
 }
+/*
+//GbifARRAY n nono no
+type GbifARRAY struct{
 
+}
+*/
 //GbifJSON get the spices result
 type GbifJSON struct {
 	Results []SpeciesJSON `json:"results"`
 }
 
 /*
-Species represents extint species by country
+Species represents species by country
 */
 type Species struct {
 	Key       int    `json:"key"`
@@ -63,7 +71,6 @@ type Species struct {
 	Species   string `json:"species"`
 	SciName   string `json:"scientificName"`
 	CanonName string `json:"canonicalName"`
-	IsExtinct bool   `json:"extinct"`
 }
 
 //SpeciesJSON represents a temporay struct to get data from GBIF
@@ -79,12 +86,13 @@ type SpeciesJSON struct {
 }
 
 /*
-Diag represents some cool comments
+Diag represent the diagnostic tool to see API status code for
+external APIs, and our version and uptime
 	{
 		"gbif": <value>, e.g "200"
 		"restcountries": <value>, e.g "200"
 		"version": <value>, e.g "v1"
-		"uptime": <value>, e.g "50.5"
+		"uptime": <value>, e.g "1200 seconds"
 	}
 */
 type Diag struct {
@@ -143,30 +151,53 @@ func speciesHandler(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 		if parts[4] == "" {
-			//Get all
-		} else if _, err := strconv.Atoi(parts[4]); err == nil {
-			file, err := os.Open("Species.txt")
+			file, err := os.Open(FILENAME_S)
 			if err != nil {
 				log.Fatal(err)
 			}
-			defer file.Close()
+			
 			data, err := ioutil.ReadAll(file)
-			i := 0
-			var species []SpeciesJSON
-			for {
-				var doc SpeciesJSON
-				err := json.Unmarshal(data, &doc)
-				if err == io.EOF {
-					break
-				}
-				if err != nil {
-					log.Fatal(err)
-				}
-				species[i] = doc
-				i++
-			}
-			json.NewEncoder(w).Encode(species)
+			file.Close()
 
+			var speciesArray []GbifJSON
+			var species GbifJSON;
+
+			json.Unmarshal(data, &speciesArray)
+			for index := 0; index < len(speciesArray); index++ {
+				species.Results = append(species.Results, speciesArray[index].Results...)
+			}
+			//Sort the data by key
+			sort.SliceStable(species.Results, func (i, j int ) bool {
+				return species.Results[i].Key < species.Results[j].Key
+			})
+			json.NewEncoder(w).Encode(species)
+		} else if key, err := strconv.Atoi(parts[4]); err == nil {
+			
+			file, err := os.Open(FILENAME_S)
+			if err != nil {
+				log.Fatal(err)
+			}
+			
+			data, err := ioutil.ReadAll(file)
+			file.Close()
+
+			var speciesArray []GbifJSON
+			var species GbifJSON;
+
+			json.Unmarshal(data, &speciesArray)
+			for index := 0; index < len(speciesArray); index++ {
+				species.Results = append(species.Results, speciesArray[index].Results...)
+			}
+			//Sort the data by key
+			for index := 0; index < len(species.Results); index++ {
+				if species.Results[index].Key == key{
+					json.NewEncoder(w).Encode(species.Results[index])
+					return
+					
+				}
+			}
+			http.Error(w, "could not find the species key", http.StatusBadRequest)
+			
 		} else {
 			fmt.Print(parts[4])
 			http.Error(w, "Expecting format .../speices_key", http.StatusBadRequest)
@@ -211,36 +242,49 @@ func diagHandler(w http.ResponseWriter, r *http.Request) {
 }
 func cachingSpecies() {
 	//TODO CreateFile
-
-	file, err := os.Open("Species.txt")
+	var speciesArray []GbifJSON 
+	file, err := os.Open(FILENAME_S)
 	if err != nil {
 		//Create the OS
-		file, err = os.Create("Species.txt")
+		file, err = os.Create(FILENAME_S)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
 
 	} else {
-		//ut exsist and we need to see how old it is
-		info, err := os.Stat("Species.txt")
+		//it exsist and we need to see how old it is
+		info, err := os.Stat(FILENAME_S)
 		if err != nil {
-			//Send help
+			fmt.Println(err)
+			return
 		}
 		mtime := info.ModTime()
 		fmt.Println("Last changed:", mtime)
 		timenow := time.Now()
+		fmt.Println("Time now:", timenow)
+		fmt.Println("Is", timenow.Sub(mtime).Hours(), "larger than", 24, "?")
 		//Does not care for timezones btw
-		if timenow.Hour() > mtime.Hour()+24 {
+		if timenow.Sub(mtime).Hours() > 24 {
 			fmt.Println("Cache is old, Run Update")
+			err := os.Remove(FILENAME_S)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			file, err = os.Create(FILENAME_S)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
 
 		} else {
+			//We have the file and it's pretty new, no need to update
+			fmt.Println("Found recent cahced data")
 			return
 		}
 	}
-
-	if err != nil {
-		fmt.Println("We fucked up")
-		//We fucked up
-	}
 	datachan := make(chan []byte)
-	//data[MAXCALL];
 	var wg sync.WaitGroup
 	for i := 0; i < MAXCALL; i++ {
 		wg.Add(1)
@@ -250,24 +294,26 @@ func cachingSpecies() {
 			query := "http://api.gbif.org/v1/species/search?limit=" + limit + "&offset=" + offset
 			resp, err := http.Get(query)
 			if err != nil {
-				//We fucked up
+				fmt.Println(err)
+				return
 			}
 			data, _ := ioutil.ReadAll(resp.Body)
-			var results GbifJSON
-			//Get it in correct format
-			json.Unmarshal(data, &results)
-			data, _ = json.Marshal(results.Results)
 			datachan <- data
 		}(i)
 		go func() {
 			getdata := <-datachan
-			file.Write(getdata)
+			var gbifJSON GbifJSON
+			json.Unmarshal(getdata, &gbifJSON)
+			speciesArray = append(speciesArray, gbifJSON)
+
 			fmt.Println("We have recived data")
 			wg.Done()
 		}()
 
 	}
 	wg.Wait()
+	speciesBytes,_ := json.Marshal(speciesArray)
+	file.Write(speciesBytes)
 	fmt.Println("We are done")
 	file.Close()
 }
